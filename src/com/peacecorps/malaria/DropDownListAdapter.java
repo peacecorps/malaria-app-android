0f8400b295f82d98624a8e33fbc26f2376c78e49
ankita-kalra <technocrat.ankita@gmail.com>
2015-07-05 06:15:46 +0530

    Added Trip Indicator UI and backend

---
+++ src/com/peacecorps/malaria/DropDownListAdapter.java
@@ -0,0 +1,142 @@
+package com.peacecorps.malaria;
+
+/**
+ * Created by Ankita on 7/4/2015.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import java.util.ArrayList;
+
+public class DropDownListAdapter extends BaseAdapter {
+
+
+    private ArrayList<String> mListItems;
+    private LayoutInflater mInflater;
+    private TextView mSelectedItems;
+    private static int selectedCount = 0;
+    private static String firstSelected = "";
+    private ViewHolder holder;
+    private static String selected = "";	//shortened selected values representation
+
+    public static String getSelected() {
+        return selected;
+    }
+
+    public void setSelected(String selected) {
+        DropDownListAdapter.selected = selected;
+    }
+
+    public DropDownListAdapter(Context context, ArrayList<String> items,
+                               TextView tv) {
+        mListItems = new ArrayList<String>();
+        mListItems.addAll(items);
+        mInflater = LayoutInflater.from(context);
+        mSelectedItems = tv;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return mListItems.size();
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.trip_item_dropdown_popup, null);
+            holder = new ViewHolder();
+            holder.tv = (TextView) convertView.findViewById(R.id.itemSelectOption);
+            holder.chkbox = (CheckBox) convertView.findViewById(R.id.itemCheckBox);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.tv.setText(mListItems.get(position));
+
+        final int position1 = position;
+
+        //whenever the checkbox is clicked the selected values textview is updated with new selected values
+        holder.chkbox.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                setText(position1);
+            }
+        });
+
+        if(TripIndicatorFragmentActivity.checkSelected[position])
+            holder.chkbox.setChecked(true);
+        else
+            holder.chkbox.setChecked(false);
+        return convertView;
+    }
+
+
+    /*
+     * Function which updates the selected values display and information(checkSelected[])
+     * */
+    private void setText(int position1){
+        if (!TripIndicatorFragmentActivity.checkSelected[position1]) {
+            TripIndicatorFragmentActivity.checkSelected[position1] = true;
+            selectedCount++;
+        } else {
+            TripIndicatorFragmentActivity.checkSelected[position1] = false;
+            selectedCount--;
+        }
+
+        if (selectedCount == 0) {
+            mSelectedItems.setText(R.string.trip_select_string);
+        } else if (selectedCount == 1) {
+            for (int i = 0; i < TripIndicatorFragmentActivity.checkSelected.length; i++) {
+                if (TripIndicatorFragmentActivity.checkSelected[i] == true) {
+                    firstSelected = mListItems.get(i);
+                    break;
+                }
+            }
+            mSelectedItems.setText(firstSelected);
+            setSelected(firstSelected);
+        } else if (selectedCount > 1) {
+            for (int i = 0; i < TripIndicatorFragmentActivity.checkSelected.length; i++) {
+                if (TripIndicatorFragmentActivity.checkSelected[i] == true) {
+                    firstSelected = mListItems.get(i);
+                    break;
+                }
+            }
+            mSelectedItems.setText(firstSelected + " & "+ (selectedCount - 1) + " more");
+            setSelected(firstSelected + " & "+ (selectedCount - 1) + " more");
+        }
+    }
+
+    private class ViewHolder {
+        TextView tv;
+        CheckBox chkbox;
+    }
+
+
+
+
+
+}
--- src/com/peacecorps/malaria/TripIndicatorFragmentActivity.java
+++ src/com/peacecorps/malaria/TripIndicatorFragmentActivity.java
@@ -1,27 +1,48 @@
 package com.peacecorps.malaria;
 
-import android.app.Fragment;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+
+import com.peacecorps.malaria.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ankita on 7/3/2015.
  */
-public class TripIndicatorFragmentActivity extends FragmentActivity implements AdapterView.OnItemSelectedListener {
+public class TripIndicatorFragmentActivity extends Activity {
 
-    private Button btnInfoHub, btnHome;
-    private Spinner drugSpinner;
-    private String mDrugPicked;
+    private Button btnInfoHub, btnHome,btnGenerate;
+    private Spinner drugSpinner,locationSpinner,itemSpinner;
+    private String mDrugPicked,mLocationPicked,mItemPicked;
+    private EditText cashData,dateData,monthData,yearData;
+    public static boolean[] checkSelected;
+    private ArrayList<String> items;
+    private PopupWindow pw;
+    private boolean expanded;
 
 
 
@@ -32,9 +53,20 @@ public class TripIndicatorFragmentActivity extends FragmentActivity implements A
         btnInfoHub=(Button)findViewById(R.id.infoButton);
         btnHome=(Button)findViewById(R.id.homeButton);
         drugSpinner=(Spinner)findViewById(R.id.trip_medication_select_spinner);
+        locationSpinner=(Spinner)findViewById(R.id.trip_location_select_spinner);
+        //itemSpinner=(Spinner)findViewById(R.id.trip_packing_item_select_spinner);
+        btnGenerate=(Button)findViewById(R.id.generateButton);
+        cashData=(EditText)findViewById(R.id.trip_cash_select_editext);
+        dateData=(EditText)findViewById(R.id.trip_date);
+        monthData=(EditText)findViewById(R.id.trip_month);
+        yearData=(EditText)findViewById(R.id.trip_year);
+
+
 
         addListeners();
-        createDrugSelectionSpinner();
+        createSelectionSpinners();
+        initialize();
+        createOnItemSelectedListeners();
     }
 
 
@@ -43,7 +75,7 @@ public class TripIndicatorFragmentActivity extends FragmentActivity implements A
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplication().getApplicationContext(),MainActivity.class));
+                startActivity(new Intent(getApplication().getApplicationContext(), MainActivity.class));
             }
         });
 
@@ -55,33 +87,218 @@ public class TripIndicatorFragmentActivity extends FragmentActivity implements A
       });
 
 
+      btnGenerate.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+
+
+              String cash=cashData.getText().toString();
+              float cash_value=Float.parseFloat(cash);
+              SharedPreferenceStore.mEditor.putFloat("com.peacecorps.malaria.trip_cash_editext",cash_value).commit();
+              Log.d("TripIndicatorActivity", "Value for cash: " + cash_value);
+              String date_data=dateData.getText().toString();
+              int date=Integer.parseInt(date_data);
+              String month_data=monthData.getText().toString();
+              int month=Integer.parseInt(month_data);
+              String year_data=yearData.getText().toString();
+              int year=Integer.parseInt(year_data);
+              String formattedate=date_data+"/"+month_data+"/"+year_data;
+              SharedPreferenceStore.mEditor.putString("com.peacecorps.malaria.trip_date", formattedate).commit();
+              String chklist="";
+              for(int i=0;i<items.size();i++)
+              {
+                if(checkSelected[i]==true)
+                {
+                   chklist+=items.get(i)+" ";
+                }
+
+              }
+              SharedPreferenceStore.mEditor.putString("com.peacecorps.malaria.trip_packing_items",chklist).commit();
+              SharedPreferenceStore.mEditor.putString("com.peacecorps.malaria.trip_drug",mDrugPicked).commit();
+              SharedPreferenceStore.mEditor.putString("com.peacecorps.malaria.trip_location",mLocationPicked).commit();
+
+              mItemPicked="Trip to "+mLocationPicked+" is scheduled on "+formattedate+". Please bring "+cash+" in cash and also the following items:- \n"+chklist;
+
+              Toast.makeText(getApplicationContext(),mItemPicked,Toast.LENGTH_LONG).show();
+
+          }
+      });
 
     }
 
-    private void createDrugSelectionSpinner() {
+    private void createSelectionSpinners() {
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this, R.array.user_medicine_settings_activity_drug_array,
-                android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> drugAdapter = ArrayAdapter.createFromResource(
+                this, R.array.drug_array,
+                R.layout.trip_spinner_item);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        drugAdapter.setDropDownViewResource(R.layout.trip_spinner_popup_item);
 
-        drugSpinner.setAdapter(adapter);
+        drugSpinner.setAdapter(drugAdapter);
 
-        drugSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> locationAdapter = ArrayAdapter.createFromResource(
+                this, R.array.location_array,
+                R.layout.trip_spinner_item);
+
+        locationAdapter.setDropDownViewResource(R.layout.trip_spinner_popup_item);
+
+        locationSpinner.setAdapter(locationAdapter);
+
+        /*ArrayAdapter<CharSequence> itemAdapter = ArrayAdapter.createFromResource(
+                this, R.array.item_array,
+                R.layout.trip_spinner_item);
+
+        itemAdapter.setDropDownViewResource(R.layout.trip_spinner_popup_item);
+
+        itemSpinner.setAdapter(itemAdapter);*/
+
+
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position,
-                               long id) {
+    public void createOnItemSelectedListeners()
+    {
+        drugSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
-        mDrugPicked = parent.getItemAtPosition(position).toString();
-        Log.d("TripIndicatorActivity","Chosen"+mDrugPicked);
-        Toast.makeText(getApplicationContext(),mDrugPicked,Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                mDrugPicked = drugSpinner.getItemAtPosition(position).toString();
+                Log.d("TripIndicatorActivity", "Chosen ->" + mDrugPicked);
+                Toast.makeText(getApplicationContext(), mDrugPicked, Toast.LENGTH_SHORT).show();
+
+            }
+
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+        });
+
+        locationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                mLocationPicked = locationSpinner.getItemAtPosition(position).toString();
+                Log.d("TripIndicatorActivity", "Chosen ->" + mLocationPicked);
+                Toast.makeText(getApplicationContext(), mLocationPicked, Toast.LENGTH_SHORT).show();
+
+            }
+
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+        });
+
+
+        /*itemSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                mItemPicked = itemSpinner.getItemAtPosition(position).toString();
+                Log.d("TripIndicatorActivity", "Chosen ->" + mItemPicked);
+                Toast.makeText(getApplicationContext(), mItemPicked, Toast.LENGTH_SHORT).show();
+
+            }
+
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+        });*/
+
+
 
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
+    private void initialize(){
+
+        items = new ArrayList();
+        items.add("Drugs");
+        items.add("Mosquito Nets");
+        items.add("Ointments");
+
+        checkSelected = new boolean[items.size()];
+        for (int i = 0; i < checkSelected.length; i++) {
+            checkSelected[i] = false;
+        }
+        Log.d("TripIndicatorActivity", "Items Added, Check Selected Done");
+        LinearLayout layout1 = (LinearLayout)findViewById(R.id.tripItemSelector);
+        final TextView itemTV=(TextView)findViewById(R.id.tripSelectBox);
+        itemTV.setOnClickListener(new OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Log.d("TripIndicatorActivity", "Inside itemTV On Click Listener");
+                if (!expanded) {
+                    //display all selected values
+                    String selected = "";
+                    int flag = 0;
+                    for (int i = 0; i < items.size(); i++) {
+                        if (checkSelected[i] == true) {
+                            selected += items.get(i);
+                            selected += ", ";
+                            flag = 1;
+                        }
+                    }
+                    if (flag == 1)
+                        itemTV.setText(selected);
+                    expanded = true;
+                } else {
+                    //display shortened representation of selected values
+                    itemTV.setText(DropDownListAdapter.getSelected());
+                    expanded = false;
+                }
+            }
+        });
+        final Button btnItemDropdown=(Button)findViewById(R.id.tripCreate);
+        btnItemDropdown.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                initiatePopUp(items,itemTV);
+            }
+        });
+
     }
+
+    private void initiatePopUp(ArrayList<String> items, TextView tv){
+        LayoutInflater inflater = (LayoutInflater)TripIndicatorFragmentActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        //get the pop-up window i.e.  drop-down layout
+        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.trip_item_dropdown_list, (ViewGroup)findViewById(R.id.tripPackingItemPopUpView));
+
+        //get the view to which drop-down layout is to be anchored
+        LinearLayout layout1 = (LinearLayout)findViewById(R.id.tripItemSelector);
+        pw = new PopupWindow(layout, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, true);
+
+        //Pop-up window background cannot be null if we want the pop-up to listen touch events outside its window
+        pw.setBackgroundDrawable(new BitmapDrawable());
+        pw.setTouchable(true);
+
+        //let pop-up be informed about touch events outside its window. This  should be done before setting the content of pop-up
+        pw.setOutsideTouchable(true);
+        pw.setHeight(LayoutParams.WRAP_CONTENT);
+
+        //dismiss the pop-up i.e. drop-down when touched anywhere outside the pop-up
+        pw.setTouchInterceptor(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    pw.dismiss();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        //provide the source layout for drop-down
+        pw.setContentView(layout);
+
+        //anchor the drop-down to bottom-left corner of 'layout1'
+        pw.showAsDropDown(layout1);
+
+        //populate the drop-down list
+        final ListView list = (ListView) layout.findViewById(R.id.tripPackingItemDropDownList);
+        DropDownListAdapter adapter = new DropDownListAdapter(this, items, tv);
+        list.setAdapter(adapter);
+    }
+
+
 }
