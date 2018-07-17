6a8025dea41a6313b7b65649247c66f8648a932f
Anamika Tripathi <anamikatripathi1601@gmail.com>
2018-07-17 20:42:35 +0530

    shifted all dimensions to dimens.xml with res naming convention (#340)

--- malaria-app-android/src/main/java/com/peacecorps/malaria/ui/user_profile/UserProfileFragment.java
+++ malaria-app-android/src/main/java/com/peacecorps/malaria/ui/user_profile/UserProfileFragment.java
@@ -181,10 +181,6 @@ public class UserProfileFragment extends BaseFragment implements UserProfileCont
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnUserFragmentListener {
         // TODO: Update argument type and name
